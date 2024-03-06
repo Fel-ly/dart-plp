683e9e6bb63e5058ef4aa8c79759b91d73f3fc9d
Fel-ly <kiokofpm@gmail.com>
2024-03-07 02:46:59 +0300

    add, sub, multiply, divide functions

---
+++ week2/functions.dart
@@ -0,0 +1,45 @@
+// add function - Task 1
+int addTwo(int a, int b) {
+  int sum = a + b;
+  return sum;
+}
+
+// subtract function - Task 2
+int SubtractTwo(int a, int b){
+  int diff = a - b;
+  return diff;
+}
+
+// Multiply function - task 3
+double MultiplyTwo(double a, double b){
+  double product = a * b;
+  return product;
+}
+
+// divide function - task 4
+double divideTwo(double a, double b){
+  double quotient = a / b;
+  return quotient;
+}
+
+//main function
+void main(){
+  //for addition and subtraction
+  int num1 = 3;
+  int num2 = 8;
+  int sum = addTwo(num1, num2);
+  int diff = SubtractTwo(num1, num2);
+
+  print("The sum is $sum");
+  print("The differennce is $diff");
+
+  //for multiplication and division
+  double num3 = 45.5;
+  double num4 = 3.5;
+  double product = MultiplyTwo(num3, num4);
+  double quotient = divideTwo(num3, num4);
+
+  print("The product is $product");
+  print("The quotient is $quotient");
+}
+
