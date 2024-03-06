bea9ac982ae791451cc865048d76f640ee269d5d
Felistus <113873235+Fel-ly@users.noreply.github.com>
2024-03-07 02:06:06 +0300

    adding comments

--- week2/dataTypes.dart
+++ week2/dataTypes.dart
@@ -1,13 +1,14 @@
 import 'dart:ffi';
 
 void main(){
-  int id = 1001;
-  String name = "Zawadi";
-  double height = 163.20;
-  bool isStudent = true;
-  List<String> subjects = ["Mathematics","English","science"];
-  Map<String, int> marks = {"Mathematics": 70, "English": 50, "Science": 80};
+  int id = 1001;    // declares the id integer value
+  String name = "Zawadi";   //delaring the name variable
+  double height = 163.20;   //declaring a double variable - height
+  bool isStudent = true;    // sets boolean value
+  List<String> subjects = ["Mathematics","English","science"];    // declaring multiple values in one variable
+  Map<String, int> marks = {"Mathematics": 70, "English": 50, "Science": 80};   //marks with the subjects as key values
 
+  // printing ouutput
   print("$name is student number $id, is $height cm tall, and takes the subjects $subjects.");
   print("Active student: $isStudent");
   print("$name's scores in the last examinations: $marks");
